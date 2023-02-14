942c915ed31b0b5560c3ce87bffc270a9fa0b3af
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2023-02-14 18:05:31 +0530

    feat(extension): added initial setup for react chrome extension

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello World', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
 });
