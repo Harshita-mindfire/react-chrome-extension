a4a40e1ca2e6ad94525f4deaa0d7f897a70e6cd2
AndrewAKG <andrewakram1997@gmail.com>
2023-09-25 16:00:07 +0300

    extension boilerplate and reading html content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import "./App.css"
+import './App.css';
 
 function App() {
+  const saveHtmlContent = () => {
+    if (chrome?.tabs) {
+      console.log('inside tabs');
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.executeScript(
+          tabs[0].id!,
+          { code: 'document.documentElement.outerHTML' },
+          (response) => {
+            console.log('inside execution');
+            const htmlContent = response ? response[0] : '';
+            const blob = new Blob([htmlContent], { type: 'text/html' });
+            console.log(blob);
+          }
+        );
+      });
+    }
+  };
+
   return (
     <div className="App">
-      Hello World
+      <button onClick={saveHtmlContent}>Save HTML Content</button>
     </div>
   );
 }
